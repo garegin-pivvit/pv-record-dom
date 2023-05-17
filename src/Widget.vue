a30fb3ded8cdf360bf767f6fe45e79e86f8db910
Garegin <garegin@pivvit.com>
2023-05-17 15:25:56 +0400

    allow recording iframe elements

--- src/Widget.vue
+++ src/Widget.vue
@@ -42,7 +42,6 @@ export default {
                 emit(event) {
                     events.push(event);
                 },
-                keepIframeSrcFn: true,
                 recordCrossOriginIframes: true,
                 maskInputOptions: {
                     text: true,
